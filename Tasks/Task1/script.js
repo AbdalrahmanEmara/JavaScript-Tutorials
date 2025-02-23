bfb47485c51d0aa9856e1089617c0328b3ec1968
Emara <emara@gmail.com>
2025-02-23 11:33:30 +0200

    Finish task 5

--- Tasks/Task1/script.js
+++ Tasks/Task1/script.js
@@ -9,3 +9,20 @@ document.querySelector("h1").style.fontSize = '80px';
 console.log("%cElzero %cWeb %cSchool", "color: red; font-size: 40px;" , 
     "color: green; font-size: 40px; font-weight: bold;", 
     "color: white; background-color: blue; font-size: 40px");
+
+console.group("Group 1");
+    console.log("Message One");
+    console.log("Message Two");
+    console.group("Child Group");
+        console.log("Message One");
+        console.log("Message Two");
+        console.group("Grand Child Group");
+            console.log("Message One");
+            console.log("Message Two");
+            console.groupEnd();
+        console.groupEnd();
+    console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+    console.log("Message One");
+    console.log("Message Two");
