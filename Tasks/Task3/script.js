8564c0548b9684302d8328039cf7d506137337bd
Emara <emara@gmail.com>
2025-03-06 17:21:35 +0200

    Finish Task 3

---
+++ Tasks/Task3/script.js
@@ -0,0 +1,53 @@
+// ONE
+console.log("First Ass");
+console.log(10 * 20 + 15 % 3 + 190 + 10 - 400); // 0
+
+// TWO
+console.log("Second Ass");
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * num - num); // 6
+
+// Soultion Three
+console.log(num * --num); // 6
+
+// Soultion Four
+console.log(num * ++num); // 6
+
+// Solution Five
+console.log(num ** num / num - num); // 6
+
+// Solution Six
+console.log(num * ++num / num + --num); // 6
+
+// THREE
+console.log("Third Ass");
+let Num = "10";
+
+// Solution One
+console.log(+Num + +Num); // 20
+
+// Solution Two
+console.log(+Num + --Num + --Num - --Num); // 20
+
+// Solution Three
+console.log(--Num * --Num - +Num - +Num); // 20
+
+// Solution Four
+console.log(+Num * --Num); // 20
+
+// FOUR
+console.log("Fourth Ass")
+let points = 10;
+
+points += 3;
+
+console.log(points); // 13
+
+points -= 5;
+
+console.log(points); // 8;
