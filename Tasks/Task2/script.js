f483e363ba352e24311216459b0684a146d78d6c
Emara <emara@gmail.com>
2025-03-07 14:15:35 +0200

    Finish Task 2

---
+++ Tasks/Task2/script.js
@@ -0,0 +1,33 @@
+// ONE
+let numberOne = 10;
+let numberTwo = 20;
+
+const normalConcatenate = numberOne + '' + numberTwo;
+console.log(normalConcatenate);
+console.log(typeof normalConcatenate)
+
+let concatenatedNums = `${numberOne}${numberTwo}`;
+console.log(concatenatedNums);
+console.log(typeof concatenatedNums);
+
+console.log(numberOne + "\n\ " + numberTwo);
+console.log(`${numberOne}
+${numberTwo}`);
+
+// TWO
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+
+
+// THREE
+console.log("`I'm In\n\\\\\ \n\Love \\\\ " + '""" ' + "'''" + "\n\++ With ++\n\ \\" + '"""' + '\\' + '"""'
+  + '\n\""' + "JavaScript" + '""' + "``"
+ );
+console.log();
+
+// FOUR
+let a = 21;
+let b = 20;
+
+console.log("_" + a +"_"+b+"" + a + "_" + b + "" +a + "_" + b + "" +a + "_" + b + "_"); // _21_2021_2021_2021_20_
