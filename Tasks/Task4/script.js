d0133a934fb4d4956e7ae540fe2940d3525dd4bf
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-03-21 16:17:21 +0200

    task 4

---
+++ Tasks/Task4/script.js
@@ -0,0 +1,28 @@
+// ONE
+console.log("Ass One");
+
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(Math.round(100000.4)); // 100000
+console.log(Math.trunc(100000.9999)); // 100000
+console.log(Math.ceil(99999.1)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(10 ** 5); // 100000
+
+// TWO
+console.log("Ass Two");
+
+console.log(Math.abs(Number.MIN_SAFE_INTEGER)); // 9007199254740991
+
+// THREE
+console.log("Ass Three");
+console.log(Number.MAX_SAFE_INTEGER ); // 16
