e4ed828bd3862397efeb29764924074d9e50d1ac
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-04-21 16:39:29 +0200

    Task 13 first problem

---
+++ Tasks/Task13/script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+console.log('One');
+function getDetails(zName, zAge, zCountry) {
+  function namePattern(zName) {
+    const words = zName.split(" ");
+  
+    return `${words[0]} ${words[1][0]}.`;
+  }
+  function ageWithMessage(zAge) {
+    const words = zAge.split(" ");
+
+    return `Your Age Is ${words[0]}`;
+  }
+  function countryTwoLetters(zCountry) {
+    // convert char to ascii
+    let newch = zCountry.charCodeAt(1) - 32;
+    // convert ascii to char
+    newch = String.fromCharCode(newch);
+
+    return `You Live In ${zCountry[0]}${newch}`;
+  }
+  function fullDetails() {
+    return `Hello ${namePattern(zName)}, ${ageWithMessage(zAge)}, ${countryTwoLetters(zCountry)}`;
+  }
+  return fullDetails(); // Do Not Edit This
+}
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+// Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+// Hello Ahmed A., Your Age Is 32, You Live In SY
